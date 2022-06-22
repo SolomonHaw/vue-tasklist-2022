5bb8bc3462864c57c24bf9a29b3a25719b47a17a
SolomonHaw <salemmystic29@gmail.com>
2022-06-22 18:02:07 +0800

    version 1.1.0

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,9 @@
 <template>
-    <h3>Version 1.0.0</h3>
+    <h3>Version 1.1.0</h3>
     <p>This app is a proof-of-concept made  
        to learn about fundamental web frameworks
-       such as Vue.JS and Bootstrap. 
+       such as Vue.JS and Bootstrap.
+    <br>Now deployed on Firebase Backend. 
     </p>
     <router-link to="/home">Go Back</router-link>
 </template>
